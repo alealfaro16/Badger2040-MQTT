1d737d6f081df468b9068b8e2578b4765679bc88
Ale Alfaro <alejandro.alfaro@sibelhealth.com>
2024-06-16 15:01:32 -0700

    Added readme and gitignore

--- build/src/CMakeFiles/BadgerMQTT.dir/Users/alealfaro/pico/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_threadsafe_background.c.obj.d
+++
@@ -1,2 +0,0 @@
-src/CMakeFiles/BadgerMQTT.dir/Users/alealfaro/pico/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_threadsafe_background.c.obj: \
- /Users/alealfaro/pico/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_threadsafe_background.c
